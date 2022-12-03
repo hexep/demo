1da1e59bf9b39cd8e0981c999b341b032f1f2c2e
hexep <ihexep@gmail.com>
2022-12-03 22:48:42 +0300

    Pathetic attempts to deal with the shadows

--- app/src/main/java/com/ihexep/demo/components/items/ExploreItem.kt
+++ app/src/main/java/com/ihexep/demo/components/items/ExploreItem.kt
@@ -1,12 +1,16 @@
 package com.ihexep.demo.components.items
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,7 +21,6 @@ import androidx.compose.ui.unit.sp
 import com.ihexep.demo.model.DemoExplorables
 import com.ihexep.demo.model.Explorable
 import com.ihexep.demo.ui.theme.DemoTheme
-import com.ihexep.demo.util.advancedShadow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -36,27 +39,24 @@ fun ExploreItem(explorable: Explorable) {
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(all = 15.dp)
         ) {
-            Card(
-                shape = RoundedCornerShape(10.dp),
-                border = BorderStroke(width = 1.dp, color = Color(0xFFE8E8E8)),
-                colors = CardDefaults.cardColors(containerColor = Color(0xFFFFFFFF)),
+            Icon(
+                painter = painterResource(explorable.iconId),
+                tint = Color.Unspecified,
+                contentDescription = "${explorable.iconId}",
                 modifier = Modifier
                     .size(55.dp)
-                    .advancedShadow(
-                        color = Color(0x1AECECEC),
-                        offsetX = 5.dp, offsetY = 7.dp,
-                        shadowBlurRadius = 10.dp
+                    .background(Color.White)
+                    .shadow(
+                        elevation = 7.dp,
+                        shape = RoundedCornerShape(10.dp),
+                        ambientColor = Color(0x1AFFFDFD)
                     )
-            ) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Icon(
-                        painter = painterResource(explorable.iconId),
-                        tint = Color.Unspecified,
-                        contentDescription = "${explorable.iconId}",
-                        modifier = Modifier.align(Alignment.Center)
+                    .clip(RoundedCornerShape(10.dp))
+                    .border(
+                        border = BorderStroke(width = 1.dp, color = Color(0xFFE8E8E8)),
+                        shape = RoundedCornerShape(10.dp)
                     )
-                }
-            }
+            )
             Column(verticalArrangement = Arrangement.spacedBy(5.dp)) {
                 Text(
                     text = explorable.title,
